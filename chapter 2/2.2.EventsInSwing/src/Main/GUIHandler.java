483fe2d465056aaa8b375b9e5e992b8bcf6aa168
Hamza Afzal <ha593164@gmail.com>
2023-04-10 01:46:35 +0500

    Chapter 2 exercise committed

---
+++ chapter 2/2.2.EventsInSwing/src/Main/GUIHandler.java
@@ -0,0 +1,11 @@
+package Main;
+import GUI.NameOutputForm;
+/**
+ *
+ * @author Hamza Afzal
+ */
+public class GUIHandler {
+    public static void main(String[] args) {
+        new NameOutputForm().setVisible(true);
+    }
+}
